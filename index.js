92f53990d14f25ce5afd827bc9eed1e6d62e5e77
Osama Yawar <osamayawar@hotmail.com>
2016-02-04 10:55:30 -0500

    Completed socketio default chat application

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.get('/', function(req, res){
 
 
 io.on('connection', function(socket){
-	console.log("a user connected");
-	socket.on('disconnect', function(){
-		console.log("a user disconnected");
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+		console.log('chat msg: "' + msg + '" emitted');
 	});
 });
 
